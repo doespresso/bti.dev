e2112e89fb26c98422fd4f08c43eac06736638af
doespresso <itone.ltd@gmail.com>
2017-05-02 16:44:44 +0300

    some

--- craft2/public/assets/scripts/app.js
+++ craft2/public/assets/scripts/app.js
@@ -1,7 +1,20 @@
 Vue.use(VueMaterial);
+Vue.material.registerTheme({
+    app: {
+        primary: 'cyan'
+    },
+    about: {
+        primary: 'indigo'
+    },
+    contact: {
+        primary: 'teal'
+    }
+});
 var app = new Vue({
+    delimiters: ['${', '}'],
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'You loaded this page on ' + new Date()
     }
-})
+});
+
