805214060d746877e77d275945e4815f3f302b3c
doespresso <itone.ltd@gmail.com>
2017-05-03 15:56:36 +0300

    before navee

--- craft2/craft/config/general.php
+++ craft2/craft/config/general.php
@@ -16,6 +16,11 @@ return array(
 		'en_us' => 'http://bti2.dev/en/',
 	),
 
+	'siteName' => array(
+		'ru' => 'Бейкер Тилли Россия',
+		'en_us' => 'Baker Tilly Russia',
+	),
+
 	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
 	'environmentVariables' => array(
 		'basePath' => '/users/jd/vh/bti.dev/craft2/public/',
@@ -37,5 +42,23 @@ return array(
 
 	// Dev Mode (see https://craftcms.com/support/dev-mode)
 	'devMode' => true,
+
+//	'limitAutoSlugsToAscii' => true,
+//    'customAsciiCharMappings' => array(
+//		// Russian
+//		'X'=>'%%%', ord('а') => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
+//		'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+//		'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+//		'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
+//		'Я' => 'Ya',
+//		'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+//		'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+//		'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+//		'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
+//		'я' => 'ya',
+//    ),
+//
+
+
 
 );
