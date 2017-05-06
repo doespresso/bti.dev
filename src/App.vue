170e3a13618162623f25f80ce26f34b3582c9e39
doespresso <itone.ltd@gmail.com>
2017-05-06 16:51:54 +0300

    webpack 1

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h1></h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs -------dsjfhsdhf</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
@@ -39,6 +39,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 
 h1, h2 {
   font-weight: normal;
