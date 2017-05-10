da28afec5b4c7a2ed4768cf552599411b85596eb
doespresso <itone.ltd@gmail.com>
2017-05-10 21:46:30 +0300

    WORK

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import './../app/styles/index.scss'
 import Vue from 'vue'
 import VueMaterial from 'vue-material/src/'
 
-
 Vue.use(VueMaterial)
+
+Vue.material.registerTheme('default', {
+  primary: 'green',
+  accent: 'white',
+  warn: 'purple',
+  background: 'white'
+})
+
 
 new Vue({
   el: '#app',
