a8893a5e70f8f10dea6843d08bf9259987a7c19d
doespresso <itone.ltd@gmail.com>
2017-05-10 15:23:00 +0300

    vue webpack-simple

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueMaterial from 'vue-material'
+
+Vue.use(VueMaterial)
+
+new Vue({
+  el: '#app',
+  data:{
+    message:'hello'
+  }
+})
