c983d529bd5ad4041dbe6124f69f20b397214fce
doespresso <itone.ltd@gmail.com>
2017-05-13 20:42:18 +0300

    before restart

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-
-
 import Vue from 'vue'
 import VueMaterial from 'vue-material/src/'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
@@ -7,39 +5,45 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.use(VueMaterial)
 Vue.use(VueAwesomeSwiper)
 
-Vue.material.registerTheme('default', {
-  primary: 'red',
-  accent: 'black',
-  warn: 'black',
-  background: 'white',
+Vue.material.registerTheme({
+    default: {
+        primary: {
+            color: 'blue',
+            hue: 500
+        },
+        accent: {
+            color: 'grey',
+            hue: 400
+        },
+    },
 })
 
 
 var app = new Vue({
-  delimiters: ['${', '}'],
-  el: '#app',
-  data: {
-    message: 'You loaded this page on ' + new Date(),
-    show:false
-  },
-  methods: {
-    toggleLeftSidenav: function () {
-      this.$refs.leftSidenav.toggle();
+    delimiters: ['${', '}'],
+    el: '#app',
+    data: {
+        message: 'You loaded this page on ' + new Date(),
+        show: false
     },
-    toggleRightSidenav: function () {
-      this.$refs.rightSidenav.toggle();
-      this.$refs.leftSidenav.close();
-    },
-    closeRightSidenav: function () {
-      this.$refs.rightSidenav.close();
-    },
-    open: function (ref) {
-      console.log('Opened: ' + ref);
-    },
-    close: function (ref) {
-      console.log('Closed: ' + ref);
+    methods: {
+        toggleLeftSidenav: function () {
+            this.$refs.leftSidenav.toggle();
+        },
+        toggleRightSidenav: function () {
+            this.$refs.rightSidenav.toggle();
+            this.$refs.leftSidenav.close();
+        },
+        closeRightSidenav: function () {
+            this.$refs.rightSidenav.close();
+        },
+        open: function (ref) {
+            console.log('Opened: ' + ref);
+        },
+        close: function (ref) {
+            console.log('Closed: ' + ref);
+        }
     }
-  }
 });
 
 import './../app/styles/index.scss'
