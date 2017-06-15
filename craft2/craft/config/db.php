57ae8e9ccbf725f32b0e6d55c43c1498b3afcb4b
doespresso <itone.ltd@gmail.com>
2017-06-15 19:53:55 +0300

    server

--- craft2/craft/config/db.php
+++ craft2/craft/config/db.php
@@ -8,20 +8,21 @@
  */
 
 return array(
-
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The name of the database to select.
-	'database' => 'bti2.dev',
-
-	// The database username to connect with.
-	'user' => 'bti2.dev',
-
-	// The database password to connect with.
-	'password' => 'bti2.dev',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
-
+    '*' => array(
+        'tablePrefix' => 'craft',
+    ),
+    '.dev'  => array(
+        'server' => 'localhost',
+        'database' => 'bti2.dev',
+        'user' => 'bti2.dev',
+        'password' => 'bti2.dev',
+        'tablePrefix' => 'craft',
+    ),
+    '.ip' => array(
+        'server' => 'localhost',
+        'database' => 'bti2.dev',
+        'user' => 'bti2.dev',
+        'password' => 'bti2.dev',
+        'tablePrefix' => 'craft',
+    )
 );
--- craft2/craft/config/general.php
+++ craft2/craft/config/general.php
@@ -1,64 +1,53 @@
 <?php
 
-/**
- * General Configuration
- *
- * All of your system's general configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
- */
 
 return array(
+    '*'  => array(
+        'defaultWeekStartDay' => 0,
+        'enableCsrfProtection' => true,
+        'omitScriptNameInUrls' => true,
+        'cpTrigger' => 'admin',
+    ),
+    '.dev' => array(
+        //	'cacheMethod' => "db",
 
+        'siteUrl' => array(
+            'ru' => 'http://bti2.dev/',
+            'en_us' => 'http://bti2.dev/en/',
+        ),
+        'siteName' => array(
+            'ru' => 'Бейкер Тилли Россия',
+            'en_us' => 'Baker Tilly Russia',
+        ),
+        'environmentVariables' => array(
+            'basePath' => '/users/jd/vh/bti.dev/craft2/public/',
+            'baseUrl' => 'http://bti2.dev/',
+        ),
+        'devMode' => true,
+        //	'limitAutoSlugsToAscii' => true,
+        //    'customAsciiCharMappings' => array(
+        //		208=>'A'
+        //	),
+    ),
+    '.ip'  => array(
+        //	'cacheMethod' => "db",
 
-
-	'siteUrl' => array(
-		'ru' => 'http://bti2.dev/',
-		'en_us' => 'http://bti2.dev/en/',
-	),
-
-	'siteName' => array(
-		'ru' => 'Бейкер Тилли Россия',
-		'en_us' => 'Baker Tilly Russia',
-	),
-
-	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
-	'environmentVariables' => array(
-		'basePath' => '/users/jd/vh/bti.dev/craft2/public/',
-		'baseUrl' => 'http://bti2.dev/',
-	),
-
-
-	// Default Week Start Day (0 = Sunday, 1 = Monday...)
-	'defaultWeekStartDay' => 0,
-
-	// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-	'enableCsrfProtection' => true,
-
-	// Whether "index.php" should be visible in URLs (true, false, "auto")
-	'omitScriptNameInUrls' => true,
-
-	// Control Panel trigger word
-	'cpTrigger' => 'admin',
-
-	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => true,
-
-//	'limitAutoSlugsToAscii' => true,
-//    'customAsciiCharMappings' => array(
-//		// Russian
-//		'X'=>'%%%', ord('а') => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
-//		'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
-//		'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
-//		'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
-//		'Я' => 'Ya',
-//		'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
-//		'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
-//		'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
-//		'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
-//		'я' => 'ya',
-//    ),
-//
-
-
-
+        'siteUrl' => array(
+            'ru' => 'http://bti2.dev/',
+            'en_us' => 'http://bti2.dev/en/',
+        ),
+        'siteName' => array(
+            'ru' => 'Бейкер Тилли Россия',
+            'en_us' => 'Baker Tilly Russia',
+        ),
+        'environmentVariables' => array(
+            'basePath' => '/users/jd/vh/bti.dev/craft2/public/',
+            'baseUrl' => 'http://bti2.dev/',
+        ),
+        'devMode' => true,
+        //	'limitAutoSlugsToAscii' => true,
+        //    'customAsciiCharMappings' => array(
+        //		208=>'A'
+        //	),
+    )
 );
